988c172ed9887bdc2ba125c85756bba284ecf6f9
Kenwoooooo <s2213019@u.tsukuba.ac.jp>
2024-05-20 15:44:36 +0900

    /user/selfと/submitという２つのエンドポイントを作りました。 時間はunix timestampというフォーマットでの入力でお願いします。
    WU BOQIAN

--- yoshapp-server/app.py
+++ yoshapp-server/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
+import datetime
 
+from flask import Flask, render_template, request
+import json
 app = Flask(__name__)
 
 
@@ -8,32 +10,27 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/user/self', methods=['POST'])
+def user():
+    if request.method == 'POST':
+        user_id = int(request.form['id'])
+        name = request.form['name']
+        birthday = request.form['birthday']
+        sex = int(request.form['sex'])
+
+        return json.dumps({'id': user_id, 'name': name, 'birthday': datetime.datetime.fromtimestamp(int(birthday)).strftime("%Y-%m-%D"), 'sex': sex})
+
+
 @app.route('/submit', methods=['POST'])
 def submit():
     if request.method == 'POST':
-        data0 = request.form['data0']
-        data1 = request.form['data1']
-        data2 = request.form['data2']
-        data3 = request.form['data3']
-        data4 = request.form['data4']
-        data5 = request.form['data5']
-        data6 = request.form['data6']
-        data7 = request.form['data7']
-        data8 = request.form['data8']
-        data9 = request.form['data9']
-        data10 = request.form['data10']
-        data11 = request.form['data11']
-        data12 = request.form['data12']
-        data13 = request.form['data13']
-        data14 = request.form['data14']
-        data15 = request.form['data15']
-        data16 = request.form['data16']
-        data17 = request.form['data17']
-        data18 = request.form['data18']
-        data19 = request.form['data19']
-        data20 = request.form['data20']
+        date = request.form['date']
+        bed_time = request.form['bed_time']
+        wake_up_time = request.form['wake_up_time']
+        memo = request.form['memo']
 
-        return f'Data from form: {data0}, {data1}, {data2}, {data3}, {data4}, {data5}, {data6}, {data7}, {data8}, {data9}, {data10}, {data11}, {data12}, {data13}, {data14}, {data15}, {data16}, {data17}, {data18}, {data19}, {data20}'
+        return json.dumps({'date': datetime.datetime.fromtimestamp(int(date)).strftime("%Y-%m-%D"), 'bed_time': datetime.datetime.fromtimestamp(int(bed_time)).strftime("%H:%M:%S"), 'wake_up_time': datetime.datetime.fromtimestamp(int(wake_up_time)).strftime("%H:%M:%S"), 'memo': memo})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
