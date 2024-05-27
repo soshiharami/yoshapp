1ece1bc4e3455bef12b30a3dd26cd20d6358ad87
taichiaaa <s2210247@s.tsukuba.ac.jp>
2024-05-27 15:42:34 +0900

    余計な空白が入っていたので削除

--- yoshapp-server/app.py
+++ yoshapp-server/app.py
@@ -7,76 +7,75 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 
- @app.route('/sleep/<id>')
- def sleep_id(id):
-     result = {}
-     limit = request.args['limit']
-     offset = request.args['offset']
-     dbname = 'database.db'
-     conn = sqlite3.connect(dbname)
-     cur = conn.cursor()
+@app.route('/sleep/<id>')
+def sleep_id(id):
+    result = {}
+    limit = request.args['limit']
+    offset = request.args['offset']
+    dbname = 'database.db'
+    conn = sqlite3.connect(dbname)
+    cur = conn.cursor()
+    cur.execute('select bed_time, wake_up_time, memo, score from data where id = ? limit ? offset ?', (id,limit,offset))
 
-     cur.execute('select bed_time, wake_up_time, memo, score from data where id = ? limit ? offset ?', (id,limit,offset))
+    sleep_list = []
+    for row in cur:
+        dict = {}
+        dict['bed_time'] = row[0]
+        dict['wake_up_time'] = row[1]
+        dict['memo'] = row[2]
+        dict['score'] = row[3]
+        sleep_list.append(dict)
 
-     sleep_list = []
-     for row in cur:
-         dict = {}
-         dict['bed_time'] = row[0]
-         dict['wake_up_time'] = row[1]
-         dict['memo'] = row[2]
-         dict['score'] = row[3]
-         sleep_list.append(dict)
-
-     result['sleep'] = sleep_list
-     result['limit'] = limit
-     result['offset'] = offset
-
-
-     cur.close()
-     conn.close()
-     return json.dumps(result)
-
- @app.route('/ranking')
- def ranking():
-     #DBからスコア情報を取得
-     limit = request.args['limit']
-     offset = request.args['offset']
-     return_dict = {} #user:{id,name,birthday,sex} ,rank, limit, offset
-
-     dbname = 'database.db' #データベースの名前
-     conn = sqlite3.connect(dbname)
-     cur = conn.cursor()
-
-     cur.execute('select id, score from data order by score desc limit ? offset ?', (limit, offset))     #ランキング情報を取得するSQL文
-     n = 1
-     users = []
-     for row in cur:
-         #ランキングを決める
-         user_dict = {}
-         user_dict['id'] = row[0]
-         user_dict['rank'] = n
-
-         users.append(user_dict)
-         n += 1
+    result['sleep'] = sleep_list
+    result['limit'] = limit
+    result['offset'] = offset
 
 
-     for n in range(len(users)):
-         #users[n] = user_dict
-         cur.execute('select name, birthday, sex from user where id = ?', (users[n]['id'],))
-         print()
-         for row in cur:
-             users[n]['name'] = row[0]
-             users[n]['birthday'] = row[1]
-             users[n]['sex'] = row[2]
+    cur.close()
+    conn.close()
+    return json.dumps(result)
 
-     cur.close()
-     conn.close()
+@app.route('/ranking')
+def ranking():
+    #DBからスコア情報を取得
+    limit = request.args['limit']
+    offset = request.args['offset']
+    return_dict = {} #user:{id,name,birthday,sex} ,rank, limit, offset
 
-     return_dict['user'] = users
-     return_dict['limit'] = limit
-     return_dict['offset'] = offset
+    dbname = 'database.db' #データベースの名前
+    conn = sqlite3.connect(dbname)
+    cur = conn.cursor()
 
-     return json.dumps(return_dict)    #JSON形式で返す
+    cur.execute('select id, score from data order by score desc limit ? offset ?', (limit, offset))     #ランキング情報を取得するSQL文
+    n = 1
+    users = []
+    for row in cur:
+        #ランキングを決める
+        user_dict = {}
+        user_dict['id'] = row[0]
+        user_dict['rank'] = n
+
+        users.append(user_dict)
+        n += 1
+
+
+    for n in range(len(users)):
+        #users[n] = user_dict
+        cur.execute('select name, birthday, sex from user where id = ?', (users[n]['id'],))
+        print()
+        for row in cur:
+            users[n]['name'] = row[0]
+            users[n]['birthday'] = row[1]
+            users[n]['sex'] = row[2]
+
+    cur.close()
+    conn.close()
+
+    return_dict['user'] = users
+    return_dict['limit'] = limit
+    return_dict['offset'] = offset
+
+    return json.dumps(return_dict)    #JSON形式で返す
 
 @app.route('/')
 def index():
